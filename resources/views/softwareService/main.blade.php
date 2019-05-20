a0a9c248d212a8b3cbeff96f9b2161bb3f013238
amriteshonly4u <amriteshonly4u@gmail.com>
2019-05-21 03:10:26 +0530

    some more changes in UI no backgorund no vison fixed and open chat is also fiex next footer

--- resources/views/softwareService/main.blade.php
+++ resources/views/softwareService/main.blade.php
@@ -3,14 +3,14 @@
     {{-- cursole:START --}}
     <div class="container-fluid" style="padding: 0">
         {{-- Nav Bar:START --}}
-        <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary" style="background-color: #007bff3b!important;">
+        <nav id ="navBar" class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary" style="background-color: #007bff3b!important;">
         
             <a class="navbar-brand" href="{{url('/')}}" style="font-size: 30px;font-weight: 900;">
                 <img src="https://i.imgur.com/CWMqgHO.png"  width="30" height="30"  alt="GangaServices Logo" />
                 Ganga Services
             </a>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button id="navBarToggerBtton" class="navbar-toggler realShadow" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
@@ -24,7 +24,7 @@
                             <a class="nav-link nav-text"  href="{{url('/')}}/regPartner" >Work</a>
                     </li>
 
-                    <li class="nav-item dropdown">
+                    <li class="nav-item dropdown active">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Services
                         </a>
@@ -286,6 +286,37 @@
     <script>
         $(function(){
 
+            /************* DONT REMOVE WORKING CODE :START ********************/
+            // on scroll checking nav bar color if not on top
+            $(window).on('scroll',function(){
+                addRemoveBackground();
+            })
+            function addRemoveBackground(){
+                // Geting scroll Height
+                $scrollHight = $(window).scrollTop();
+
+                // adding class when scroll hight is grater 
+                if($scrollHight>376){
+                    $("#navBar").removeAttr('style');
+                }else{
+                    $("#navBar").attr('style','background-color: #007bff3b!important;');
+                }
+            }
+            /************* DONT REMOVE WORKING CODE :END ********************/
+
+
+            // nav bar color on togger button for mobiel only
+            $("#navBarToggerBtton").click(function(){
+                $iscollapsed = $("#navbarNav").hasClass('show')
+                console.log($iscollapsed);
+                if($iscollapsed){
+                    $("#navBar").attr('style','background-color: #007bff3b!important;');
+                    addRemoveBackground();
+                }else{
+                    $("#navBar").removeAttr('style');
+                    // addRemoveBackground();
+                }
+            });
             // Starting slide bear for happy clints
             $('.autoplay').slick({
                 slidesToShow: 3,
@@ -301,11 +332,26 @@
             // opening twak chat on request
             $(".startChart").click(function(){
                 console.log("clicked startchat");
-                document.getElementById("bubble-image").click();
 
-                // $("#maximizeChat").click();
-                $("#chatTextarea").val("Hello! I need some help.");
-                $("#textareaSubmitButton").click();
+                //checking if mobile
+                var isMobile = false; //initiate as false
+                // device detection
+                if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+                    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+                    isMobile = true;
+                }
+
+                // Selectin iframe body
+                var tawkWindow = document.getElementsByTagName('iframe')[0].contentWindow.document;
+                if(isMobile){
+                    tawkWindow = document.getElementsByTagName('iframe')[1].contentWindow.document;
+                }
+                // writing message
+                tawkWindow.getElementById("chatTextarea").value = 'Hello! I need some help.';
+                // tawkWindow.getElementById("chatTextarea").focus();
+                // $submitBtn = tawkWindow.getElementById("chatTextarea");
+                // var e = $.Event( "keypress", { which: 13 } );
+                // $('tawkWindow #chatTextarea').trigger(e);
             });
         })
     </script>
