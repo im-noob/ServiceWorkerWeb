e0e5cc1b4ba021048f73781dbb434d3850ac915b
amriteshonly4u <amriteshonly4u@gmail.com>
2019-05-26 20:33:32 +0530

    next ratting to rating spellign mistake

--- resources/views/productDetails.blade.php
+++ resources/views/productDetails.blade.php
@@ -25,11 +25,11 @@
     </section>
     {{-- Info:END --}}
 
-
+    {{-- ADD To CArt and Continus Cart BUTTON:START --}}
     <section>
-        <div class="container-fluid" style="padding: 0px">
+        <div class="container" style="padding: 0px">
             <div class="card" style="align-items: center;">
-                <div class="row">
+                <div class="row" style="width: 100%;">
                     <div class="col-6" style="align-self: center;">
                         <span><i class="fas fa-rupee-sign" id="price{{$DetailsList->wor_list_id}}">{{$DetailsList->price}}</i> /- </span>
                             
@@ -50,7 +50,7 @@
                         {{-- Importing Add to Cart Button:END --}}
                     </div>
                 </div>
-                <div class="card-footer">
+                <div class="card-footer" style="width: 100%;">
                     <div class="row">
                         <div class="col-sm-6 "  style="align-self:center">
                                 <span style="font-size: x-large;"> Total : <i class="fas fa-rupee-sign" id="total">00</i></span>
@@ -65,6 +65,33 @@
             </div>
         </div>
     </section>
+    {{-- ADD To CArt and Continus Cart BUTTON:END --}}
+    
+    {{-- Latet Customer Review:START --}}
+    <section>
+        <div class="container card">
+            <h3 style="font-weight: 700">Latest Customer Review</h3>
+            <span class="text-muted">For {{$DetailsList->work_name}}   </span>
+            <section style="background-color: #f5f5f5;border-radius: 4px">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <div class="col-sm-10">
+                        <div class="row">
+                            <span style="font-weight: 700;">4.7 out of 5 start</span>
+                            <span class="text-muted">average rating </span>
+                        </div>
+                        <div class="row">
+                            <span style="font-weight: 700;">17,765 review</span>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+        </div>
+    </section>
+    {{-- Latet Customer Review:END --}}
     <script>
         var AllCartItemWorkListID = [];
         AllCartItemWorkListID.push({{$DetailsList->wor_list_id}});
